9a97e33a186363ce087c63d788d1fa698cb97c2b
Taofeek Ajibade <117765480+Haysquaretech@users.noreply.github.com>
2023-03-13 22:04:52 -0800

    Create meal.py

---
+++ meal.py
@@ -0,0 +1,21 @@
+import random
+
+"""
+This list itemizes each category of food, it may be rearranged to your taste.
+It is a dynamic list and may never be able to satisfy particular standards for completeness. 
+You can help by adding missing items.
+"""
+
+list1 = "Rice", "Yam", "Pasta", "Noodles", "Semo with Efo Riro", "Amala with Ewedu", "Jollof Rice"
+
+list2 = "Plantain", "Snacks", "Bread and Beans", "Snacks and Soda", "Iyan", "Puff-Puff with Malt"
+
+list3 = "Potato", "Carrot", "Bacon and eggs", "Porridge", "Bread with Akara", "Moi Moi with Pap"
+
+breakfast = random.choice(list1)
+lunch = random.choice(list2)
+dinner = random.choice(list3)
+
+print(f"{breakfast} is for breakfast")
+print(f"{lunch} is for lunch")
+print(f"{dinner} is for dinner")
